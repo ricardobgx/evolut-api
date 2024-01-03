9e498596637b55a3eaca7adae40e57ff2cb33c39
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 16:33:25 -0300

    feat(user): add user details dto

---
+++ src/main/java/com/evolut/api/domain/user/dtos/UserDetails.java
@@ -0,0 +1,16 @@
+package com.evolut.api.domain.user.dtos;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(of = "id", callSuper = false)
+public class UserDetails extends RepresentationModel<UserDetails> {
+    private UUID id;
+    private String name;
+    private String email;
+    private UUID profilePictureId;
+}
