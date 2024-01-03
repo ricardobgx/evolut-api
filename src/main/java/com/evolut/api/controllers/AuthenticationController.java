e4075f9a26e0eb0b99c2ac06c54d0c9826af1e95
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 16:24:35 -0300

    feat(security): add authentication controller

---
+++ src/main/java/com/evolut/api/controllers/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.evolut.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("authentication")
+public class AuthenticationController {
+}
