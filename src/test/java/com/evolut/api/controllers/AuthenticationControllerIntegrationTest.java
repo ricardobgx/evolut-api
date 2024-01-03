6210cf8c7c4893d7da1ac263a92a0ec6cdd300ea
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 15:28:20 -0300

    test(authentication): add test annotations to authentication controller integration test class

--- src/test/java/com/evolut/api/controllers/AuthenticationControllerIntegrationTest.java
+++ src/test/java/com/evolut/api/controllers/AuthenticationControllerIntegrationTest.java
@@ -1,4 +1,13 @@
 package com.evolut.api.controllers;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
 public class AuthenticationControllerIntegrationTest implements AuthenticationControllerTest {
 }
