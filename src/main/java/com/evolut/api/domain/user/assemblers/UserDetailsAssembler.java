d48611e3f66b275c053150eb5a488ec09e91e70a
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 16:49:48 -0300

    feat(user): add assembler to user details dto

---
+++ src/main/java/com/evolut/api/domain/user/assemblers/UserDetailsAssembler.java
@@ -0,0 +1,17 @@
+package com.evolut.api.domain.user.assemblers;
+
+import com.evolut.api.domain.user.User;
+import com.evolut.api.domain.user.dtos.UserDetails;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsAssembler implements RepresentationModelAssembler<User, EntityModel<UserDetails>> {
+    @Override
+    public EntityModel<UserDetails> toModel(User user) {
+        UserDetails userDetails = new UserDetails(user);
+
+        return EntityModel.of(userDetails);
+    }
+}
