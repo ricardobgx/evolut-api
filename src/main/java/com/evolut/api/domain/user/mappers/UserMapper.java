b6fd2b2ece5009d8ad44cb0a7b2817639abf1e0c
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 17:30:15 -0300

    feat(user): add method to mapper to map create user input dto to user model

--- src/main/java/com/evolut/api/domain/user/mappers/UserMapper.java
+++ src/main/java/com/evolut/api/domain/user/mappers/UserMapper.java
@@ -1,7 +1,10 @@
 package com.evolut.api.domain.user.mappers;
 
+import com.evolut.api.domain.user.dtos.CreateUserInput;
+import com.evolut.api.domain.user.models.User;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface UserMapper {
+    User fromCreateUserInput(CreateUserInput createUserInput);
 }
