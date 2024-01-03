f37718134e54dc2eb63370810bb73be7d6f53cc4
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 17:51:11 -0300

    test(user): add method to user controller test interface to test sign up route when correct input was provided

--- src/test/java/com/evolut/api/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/evolut/api/controllers/AuthenticationControllerTest.java
@@ -1,4 +1,5 @@
 package com.evolut.api.controllers;
 
 public interface AuthenticationControllerTest {
+    void givenCorrectInputWhenSignUpThenReturnHttpStatus201AndUserCreated() throws Exception;
 }
