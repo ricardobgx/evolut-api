15658bc4c9a35695d85d0b3170cc9e63ed27c084
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 16:22:09 -0300

    feat(security): change session management to stateless

--- src/main/java/com/evolut/api/infra/security/configurations/SecurityConfiguration.java
+++ src/main/java/com/evolut/api/infra/security/configurations/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 }
