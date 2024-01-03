70b73f01d3556780f9bf95f1091022328aa2b69d
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 17:32:16 -0300

    test(user): implement method in create user service unit test class to test service execution when correct input was provided

--- src/test/java/com/evolut/api/domain/user/services/CreateUserServiceUnitTest.java
+++ src/test/java/com/evolut/api/domain/user/services/CreateUserServiceUnitTest.java
@@ -1,9 +1,41 @@
 package com.evolut.api.domain.user.services;
 
+import com.evolut.api.domain.user.dtos.CreateUserInput;
+import com.evolut.api.domain.user.models.User;
+import com.evolut.api.domain.user.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class CreateUserServiceUnitTest implements CreateUserServiceTest {
+    @Autowired
+    private CreateUserService createUserService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    @Override
+    public void givenCorrectInputWhenExecuteThenReturnUserCreated() {
+        CreateUserInput createUserInput = new CreateUserInput(
+                "User 1",
+                "user1@email.com",
+                "User1@123"
+        );
+
+        when(this.userRepository.save(any())).thenReturn(Mockito.mock(User.class));
+
+        User user = this.createUserService.execute(createUserInput);
+
+        assertThat(user).isNotNull();
+    }
 }
