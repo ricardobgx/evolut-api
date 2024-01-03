e70453fee11d32270ddc47c6189b6708b0ac1e68
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 17:05:53 -0300

    test(user): add test annotations to create user service unit test class

--- src/test/java/com/evolut/api/domain/user/services/CreateUserServiceUnitTest.java
+++ src/test/java/com/evolut/api/domain/user/services/CreateUserServiceUnitTest.java
@@ -1,4 +1,9 @@
 package com.evolut.api.domain.user.services;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class CreateUserServiceUnitTest implements CreateUserServiceTest {
 }
