fc9642a998bbc5a6dfcbb89bc1a9d186dca84929
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 16:36:54 -0300

    fix(user): add annotations to create constructors with all and no one argument to user details dto

--- src/main/java/com/evolut/api/domain/user/dtos/UserDetails.java
+++ src/main/java/com/evolut/api/domain/user/dtos/UserDetails.java
@@ -1,12 +1,16 @@
 package com.evolut.api.domain.user.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id", callSuper = false)
 public class UserDetails extends RepresentationModel<UserDetails> {
     private UUID id;
