de9bba9b0a5d57c2e8c3f4e9aa7b10406fc28cdf
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 15:28:07 -0300

    test(authentication): add test annotations to authentication controller unit test class

--- src/test/java/com/evolut/api/controllers/AuthenticationControllerUnitTest.java
+++ src/test/java/com/evolut/api/controllers/AuthenticationControllerUnitTest.java
@@ -1,4 +1,11 @@
 package com.evolut.api.controllers;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class AuthenticationControllerUnitTest implements AuthenticationControllerTest {
 }
