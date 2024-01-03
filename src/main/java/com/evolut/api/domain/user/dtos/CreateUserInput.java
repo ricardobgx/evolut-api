a23dd8777f62398cdd2fcab43f72899dba0d0595
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 16:33:17 -0300

    feat(user): add create user input dto

---
+++ src/main/java/com/evolut/api/domain/user/dtos/CreateUserInput.java
@@ -0,0 +1,16 @@
+package com.evolut.api.domain.user.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
+public record CreateUserInput(
+        @NotBlank
+        String name,
+        @NotBlank
+        String email,
+        @NotBlank
+        String password,
+        UUID profilePictureId
+) {
+}
