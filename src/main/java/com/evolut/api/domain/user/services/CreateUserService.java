8b1611b171c9a51e3d10bafb0443d049817c7050
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 17:30:50 -0300

    feat(user): add method to create user service to execute business logic

--- src/main/java/com/evolut/api/domain/user/services/CreateUserService.java
+++ src/main/java/com/evolut/api/domain/user/services/CreateUserService.java
@@ -1,7 +1,25 @@
 package com.evolut.api.domain.user.services;
 
+import com.evolut.api.domain.user.dtos.CreateUserInput;
+import com.evolut.api.domain.user.mappers.UserMapper;
+import com.evolut.api.domain.user.models.User;
+import com.evolut.api.domain.user.repositories.UserRepository;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateUserService {
+    private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User execute(CreateUserInput createUserInput) {
+        User user = this.userMapper.fromCreateUserInput(createUserInput);
+
+        this.userRepository.save(user);
+
+        return user;
+    }
 }
