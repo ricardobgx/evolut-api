fa7569f055a54a6a1cf0b5f5dbeb08293c059b91
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 17:32:41 -0300

    test(user): implement method in create user service integration test class to test service execution when correct input was provided

--- src/test/java/com/evolut/api/domain/user/services/CreateUserServiceIntegrationTest.java
+++ src/test/java/com/evolut/api/domain/user/services/CreateUserServiceIntegrationTest.java
@@ -1,11 +1,33 @@
 package com.evolut.api.domain.user.services;
 
+import com.evolut.api.domain.user.dtos.CreateUserInput;
+import com.evolut.api.domain.user.models.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 public class CreateUserServiceIntegrationTest implements CreateUserServiceTest {
+    @Autowired
+    private CreateUserService createUserService;
+
+    @Test
+    @Override
+    public void givenCorrectInputWhenExecuteThenReturnUserCreated() {
+        CreateUserInput createUserInput = new CreateUserInput(
+                "User 1",
+                "user1@email.com",
+                "User1@123"
+        );
+
+        User user = this.createUserService.execute(createUserInput);
+
+        assertThat(user).isNotNull();
+    }
 }
