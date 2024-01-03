8b0a2b62c6d32162faefb20df516f2c960f6f5b4
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 17:05:44 -0300

    test(user): add test annotations to create user service integration test class

--- src/test/java/com/evolut/api/domain/user/services/CreateUserServiceIntegrationTest.java
+++ src/test/java/com/evolut/api/domain/user/services/CreateUserServiceIntegrationTest.java
@@ -1,4 +1,11 @@
 package com.evolut.api.domain.user.services;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
 public class CreateUserServiceIntegrationTest implements CreateUserServiceTest {
 }
