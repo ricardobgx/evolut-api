3813f9904431c6f30294299cc197240122006741
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-01-03 11:50:21 -0300

    feat: add project files

---
+++ src/test/java/com/evolut/api/EvolutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolut.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
